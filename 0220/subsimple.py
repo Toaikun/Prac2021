c7f7340563959a2bf8307b2a201d6c2ce9ffcc08
Nikita Bogdanov <n.o.bogdanov@tinkoff.ru>
2021-06-04 00:17:28 +0300

    Add git button

--- 0220/subsimple.py
+++ 0220/subsimple.py
@@ -10,13 +10,18 @@ class Application(tk.Frame):
     def createWidgets(self):
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.dirButton = tk.Button(self, text='dir', command=self.dir)
+        self.gitButton = tk.Button(self, text='git', command=self.git)
         self.outputLabel = tk.Label(self, text = "<result>")
         self.dirButton.grid()
-        self.quitButton.grid(row = 0, column = 1)
-        self.outputLabel.grid(row=1, columnspan = 2)
+        self.gitButton.grid(row = 0, column = 1)
+        self.quitButton.grid(row = 0, column = 2)
+        self.outputLabel.grid(row=1, columnspan = 3)
 
     def dir(self):
     	self.outputLabel["text"] = subprocess.run("dir", capture_output=True).stdout
+
+    def git(self):
+        self.outputLabel["text"] = subprocess.run("git", capture_output=True).stdout
 
 app = Application()
 app.master.title('dir/date/git')
